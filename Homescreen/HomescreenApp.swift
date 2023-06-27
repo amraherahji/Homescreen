f2da9493cf2b98a4d98a59fa710778b2d2978d23
Amrah Rahji <arasheedrahji@gmail.com>
2023-06-27 09:37:39 -0400

    Initial Commit

---
+++ Homescreen/HomescreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomescreenApp.swift
+//  Homescreen
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HomescreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
